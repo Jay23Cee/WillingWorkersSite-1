b21762db6d437e0252c7f4d2144b2225ddfa24e3
JAYCEE <Jayceexxiii@gmail.com>
2022-10-14 23:50:45 -0700

    adding google map

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -1,14 +1,51 @@
 import React from 'react'
+import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api';
 
-const Contact =()=>{
+const containerStyle = {
+  width: '400px',
+  height: '400px',
+  
+};
+
+const center = {
+  lat: 34.03987169154813,
+  lng:  -118.34399681590634
+};
+
+
+
+const onLoad = (marker: any) => {
+  console.log('marker: ', marker)
+}
+
+function Contact() {
+
+
+
   return (
-    <div>
 
-    <h2>Contact page</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos aperiam, necessitatibus saepe aliquid autem quae possimus repellat iure obcaecati vero nemo placeat. Quod possimus fugiat mollitia inventore magni, necessitatibus impedit.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam esse ipsa perspiciatis laboriosam saepe reprehenderit nemo, a iste. Praesentium vel ut labore fugit?</p>
-        </div>
+    <div className="Gmap">
+
+   
+    <LoadScript
+      googleMapsApiKey='AIzaSyAIQ76AHNTX5-2ZwTZj7D4G1FLPEjez6oE'
+    >
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={14    }
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <MarkerF
+      onLoad={onLoad}
+      position={center}
+    />
+        <></>
+      </GoogleMap>
+    </LoadScript>
+
+    </div>
   )
 }
 
-export default Contact
+export default React.memo(Contact)
