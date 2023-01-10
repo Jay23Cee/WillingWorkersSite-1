316e6f591d863978b43197a4955fa889b46de2a8
JAYCEE <Jayceexxiii@gmail.com>
2023-01-09 16:07:08 -0800

    added navigate function to Nav bar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -3,32 +3,42 @@ import {Nav, Navbar} from 'react-bootstrap';
 import styled from 'styled-components';
 import { Button } from 'react-bootstrap';
 
+import { useNavigate } from "react-router-dom";
 
+export const NavigationBar = () => {
+  const navigate = useNavigate();
 
-export const NavigationBar=()=> {
+  const handleClick = (path:any) => {
+    navigate(path);
+  };
+
   return (
-
     <Navbar expand="lg">
-        <Navbar.Brand href='/'>Willing Workers</Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav'/>
-        <Navbar.Collapse id="basic-navbar-nav">
-
-            <Nav className="ml-auto">
-                <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
-                <Nav.Item><Nav.Link href="/programs">Programs</Nav.Link></Nav.Item>
-                <Nav.Item><Nav.Link href="/about">About</Nav.Link></Nav.Item>
-                <Nav.Item><Nav.Link href="/contact">Contact</Nav.Link></Nav.Item>
-                <Button>Donate</Button>
-            </Nav>
-        </Navbar.Collapse>
-
-
-
+      <Navbar.Brand href="/">Willing Workers</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Item>
+            <Nav.Link onClick={() => handleClick("/")}>Home</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link onClick={() => handleClick("/programs")}>
+              Programs
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link onClick={() => handleClick("/about")}>About</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link onClick={() => handleClick("/contact")}>
+              Contact
+            </Nav.Link>
+          </Nav.Item>
+          <Button>Donate</Button>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
+  );
+};
 
-
-
-  )
-}
-
-export default NavigationBar
+export default NavigationBar;
