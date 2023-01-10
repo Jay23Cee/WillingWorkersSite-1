7b83102dcfd46285626e78a60dc470111e47f6c1
JAYCEE <Jayceexxiii@gmail.com>
2023-01-09 19:44:04 -0800

    updated contacts

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -1,49 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   GoogleMap,
   InfoWindow,
   LoadScript,
   MarkerF,
+  DirectionsRenderer,
 } from "@react-google-maps/api";
 import styled from "styled-components";
 import { Footer } from "./Footer";
-
-const Style = styled.div`
-  .contact {
-    display: flex;
-    flex-direction: row;
-  }
-  .contact-text {
-    width: 20vw;
-    h3 {
-      font-size: 0.9em;
-    }
-  }
-  .Gmap {
-    width: 80vw;
-  }
-  @media screen and (max-width: 768px) {
-    .contact {
-      flex-direction: column;
-    }
-    .Gmap {
-      width: 100vw;
-    }
-    .contact-text {
-      width: 60vw;
-    }
-  }
-`;
 
 const containerStyle = {
   width: "100%",
   height: "100%",
   /* Other styles for the map container */
 };
+const options = {
+  disableDefaultUI: true,
+  gestureHandling: "none",
+};
 
 function Contact() {
+  const [selectedMarker, setSelectedMarker] = useState<{
+    lat: number;
+    lng: number;
+  } | null>(null);
   const center = {
     lat: 34.03987169154813,
+    lng: -118.34399681590634,
+  };
+
+  const Popup_map = {
+    lat: 34.04287169154919,
     lng: -118.34399681590634,
   };
 
@@ -53,12 +40,20 @@ function Contact() {
 
   var customLabel = {
     text: "A",
-
     color: "#636363",
     fontSize: "10px",
     fontWeight: "bold",
     top: "20px",
   };
+
+  // Add state variable to store the state of the InfoWindow
+  const [isInfoOpen, setIsInfoOpen] = useState(false);
+
+  // Add a state variable to store the current marker that is clicked
+  const [currentMarker, setCurrentMarker] = useState<{
+    lat: number;
+    lng: number;
+  } | null>(null);
 
   return (
     <div className="contact">
@@ -74,6 +69,7 @@ function Contact() {
               mapContainerStyle={containerStyle}
               center={center}
               zoom={14}
+              options={options}
             >
               {/* Child components, such as markers, info windows, etc. */}
               <MarkerF
@@ -81,39 +77,71 @@ function Contact() {
                 position={center}
                 label={customLabel}
                 onClick={() => {
-                  console.log("Infowindow should pop");
-                  return <InfoWindow></InfoWindow>;
+                  // Update the selected marker and open the InfoWindow
+                  setSelectedMarker(center);
+                  setIsInfoOpen(true);
+                  setCurrentMarker(center);
                 }}
               ></MarkerF>
-              <></>
+              {isInfoOpen && currentMarker && (
+                <InfoWindow
+                  position={currentMarker}
+                  onCloseClick={() => {
+                    // Close the InfoWindow and clear the selected marker
+                    setIsInfoOpen(false);
+                    setCurrentMarker(null);
+                  }}
+                  zIndex={10}
+                >
+                  <div>
+                    <h5>Willing Workers</h5>
+                    <a
+                      href={`https://www.google.com/maps/dir/?api=1&destination=${encodeURI(
+                        "4813 W. Washington Blvd., Los Angeles, Los Angeles 90016"
+                      )}`}
+                      target="_blank"
+                    >
+                      <p>
+                        {" "}
+                        4813 W. Washington Blvd.<br></br>
+                      </p>
+                      <p> Los Angeles , Los Angeles 90016</p>
+                    </a>
+                  </div>
+                </InfoWindow>
+              )}
             </GoogleMap>
           </LoadScript>
         </div>
 
         <div className="contact-text">
           <h5>Schedule a tour today</h5>
-          <h3 style={{ color: "gray;" }}>
+          <h3 style={{}}>
             Willing Workers is currently giving tours following CDC guidelines.
             Masks are mandatory during your visit.
           </h3>
           <h3>
             {" "}
-            4813 W. Washington Blvd.<br></br>
-            Los Angeles , Los Angeles 90016
+            <a
+              href={`https://www.google.com/maps/dir/?api=1&destination=${encodeURI(
+                "4813 W. Washington Blvd., Los Angeles, Los Angeles 90016"
+              )}`}
+              target="_blank"
+            >
+              4813 W. Washington Blvd.<br></br>
+              Los Angeles, CA 90016
+            </a>
           </h3>
-          <br></br>
-
-          <h3>Telephone: (323) 937-5950</h3>
+          <h3>Monday - Friday 8:00am - 3:00pm</h3>
           <h3>
-            Monday - Friday <br></br> 7am-3pm
+            Phone:<a href="tel:323-729-9898">323-729-9898</a>
           </h3>
-          <br></br>
+          <h3>Email: info@willingworkers.org</h3>
         </div>
       </div>
-      <Footer/>
-
+      <Footer />
     </div>
   );
 }
 
-export default React.memo(Contact);
+export default Contact;
