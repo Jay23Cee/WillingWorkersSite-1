14e81ddf1ea6760fa27a21373af3afd7683c9f5e
JAYCEE <Jayceexxiii@gmail.com>
2023-01-15 17:35:49 -0800

    "learn more" change

--- src/Home.tsx
+++ src/Home.tsx
@@ -51,7 +51,7 @@ const Home = () => {
           </p>
           
           <Button onClick={()=>{routeProgramChange("/about")}} >
-          Learn More About Us.
+          Learn More
           </Button>
         </div>
 
